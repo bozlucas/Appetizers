64385bc0dc334add3237bae4143cf779ce2207a6
Lucas <lucasprc93@gmail.com>
2023-03-08 13:32:41 -0800

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Lucas on 2023-03-08.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
